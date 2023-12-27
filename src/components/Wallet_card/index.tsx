ecf201f9f1351344ba7da822c030ed05b2868023
fastleopard <fastleopard9372@gmail.com>
2023-12-27 15:42:03 +0900

    wallet_connect

--- src/components/Wallet_card/index.tsx
+++ src/components/Wallet_card/index.tsx
@@ -1,9 +1,124 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import types from '@/types'
+declare global {
+  interface Window {
+    unisat: any,
+    litescribe: any,
+    ordinals: any,
+    xverse: any,
+    leather: any,
+    phantom: any,
+    BitcoinProvider: any,
+    XverseProviders: any,
+    LeatherProvider: any,
+  }
+}
+
+
 const Wallet_card = (data: types.Wallet_connect) => {
+
+  const [accounts, setAccounts] = useState<string[]>([]);
+  const [network, setNetwork] = useState<string>('');
+  const [publicKey, setPublicKey] = useState<string>('');
+  const [balance, setBalance] = useState<any>(null);
+
+
+  const connectWallet = async () => {
+
+    try {
+      let accounts = await window.litescribe.requestAccounts();
+
+      setAccounts(accounts);
+    } catch (e) {
+      console.log('LiteScribe Connection failed');
+      window.open('https://litescribe.io/');
+    }
+
+    // try {
+    //   let res = await window.litescribe.getAccounts();
+
+    // } catch (e) {
+    //   console.log(e);
+    // }
+
+    // try {
+    //   let res = await window.litescribe.getNetwork();
+    //   setNetwork(res);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+
+    // try {
+    //   let res = await window.litescribe.getPublicKey();
+    //   setPublicKey(res);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+
+    // try {
+    //   let res = await window.litescribe.getBalance();
+    //   setBalance(res);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+
+
+  }
+
+  const connectUnisat = async () => {
+    console.log('unisat')
+    try {
+      let accounts = await window.unisat.requestAccounts();
+
+    }
+    catch {
+      console.log('Unisat Connection failed');
+      window.open('https://unisat.io/')
+    }
+  }
+
+  const connectXverse = async () => {
+    try {
+      let accounts = await window.XverseProviders.BitcoinProvider.connect();
+    }
+    catch {
+      console.log('Xverse Connection failed');
+      window.open('https://www.xverse.app/download');
+
+    }
+
+
+  }
+
+  const connectLeather = async () => {
+    try {
+      let accounts = await window.LeatherProvider.authenticationRequest();
+    }
+    catch {
+      console.log('Leather Connection failed');
+      window.open('https://leather.io/');
+    }
+  }
   const handleClick = (title: string, link: string) => {
-    console.log(title);
+    if (title === 'Leather') {
+      connectLeather();
+    } else if (title === 'LiteScrive') {
+      connectWallet();
+    } else if (title === 'Phantom') {
+
+    } else if (title === 'Unisat') {
+      connectUnisat();
+    } else if (title === 'Xverse') {
+      connectXverse();
+    }
+    /*
+    https://www.xverse.app/download
+https://leather.io/
+https://unisat.io/
+https://litescribe.io/
+https://phantom.app/
+    */
   }
   return (
     <div className='p-2 w-1/4 pb-6' onClick={() => handleClick(data.title, data.link)}>
@@ -30,186 +145,94 @@ export default Wallet_card
 // import logo from './logo.svg';
 // import './App.css';
 // import { useState } from 'react';
-// declare global {
-//   interface Window {
-//     unisat: any,
-//     litescribe: any,
-//     ordinals: any,
-//     xverse: any,
-//     leather: any,
-//     phantom: any,
-//     BitcoinProvider: any,
-//     XverseProviders: any,
-//     LeatherProvider: any,
-//   }
-// }
+
+
+function App() {
 
 
 
-// function App() {
-
-//   const [accounts, setAccounts] = useState<string[]>([]);
-//   const [network, setNetwork] = useState<string>('');
-//   const [publicKey, setPublicKey] = useState<string>('');
-//   const [balance, setBalance] = useState<any>(null);
 
 
-//   const connectWallet = async () => {
+  //   const connectPhantom = async () => {
+  //     console.log(window);
+  //   }
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+  //         <p>
+  //           LiteScribe wallet tester
+  //         </p>
+  //         <button onClick={() => { connectWallet() }}>
+  //           Wallet
+  //         </button>
 
-//     try {
-//       let accounts = await window.litescribe.requestAccounts();
-
-//       setAccounts(accounts);
-//     } catch (e) {
-//       console.log('LiteScribe Connection failed');
-//     }
-
-//     try {
-//       let res = await window.litescribe.getAccounts();
-
-//     } catch (e) {
-//       console.log(e);
-//     }
-
-//     try {
-//       let res = await window.litescribe.getNetwork();
-//       setNetwork(res);
-//     } catch (e) {
-//       console.log(e);
-//     }
-
-//     try {
-//       let res = await window.litescribe.getPublicKey();
-//       setPublicKey(res);
-//     } catch (e) {
-//       console.log(e);
-//     }
-
-//     try {
-//       let res = await window.litescribe.getBalance();
-//       setBalance(res);
-//     } catch (e) {
-//       console.log(e);
-//     }
+  //         <button onClick={() => { connectUnisat() }}>
+  //           Connect Unisat
+  //         </button>
 
 
-//   }
+  //         <button onClick={() => { connectXverse() }}>
+  //           Connect Xverse
+  //         </button>
 
-//   const connectUnisat = async () => {
-//     console.log('unisat')
-//     try {
-//       let accounts = await window.unisat.requestAccounts();
+  //         <button onClick={() => { connectLeather() }}>
+  //           Connect Leather
+  //         </button>
 
-//     }
-//     catch {
-//       console.log('Unisat Connection failed')
-//     }
-//   }
+  //         <button onClick={() => { connectPhantom() }}>
+  //           Connect Phantom
+  //         </button>
+  //         <button onClick={() => { window.litescribe.initialize() }}>
+  //           Disconnect
+  //         </button>
 
+  //         <p>
+  //           <label>
+  //             Address<br />
+  //           </label>
+  //           <input
+  //             value={accounts[0]}
+  //           >
+  //           </input>
+  //         </p>
 
+  //         <p>
+  //           <label>
+  //             Network<br />
+  //           </label>
+  //           <input
+  //             value={network}
+  //             disabled={true}
+  //           >
+  //           </input>
+  //         </p>
+  //         <p>
+  //           <label>
+  //             Public Key<br />
+  //           </label>
+  //           <input
+  //             value={publicKey}
+  //             disabled={true}
+  //           >
+  //           </input>
+  //         </p>
+  //         <p>
+  //           <label>
+  //             Balance<br />
+  //           </label>
+  //           <input
+  //             value={balance?.total}
+  //             disabled={true}
+  //           >
+  //           </input>
+  //         </p>
+  //       </header>
+  //       <body>
 
-//   const connectXverse = async () => {
-//     try {
-//       let accounts = await window.XverseProviders.BitcoinProvider.connect();
-//     }
-//     catch {
-//       console.log('Xverse Connection failed')
-//     }
-
-
-//   }
-
-//   const connectLeather = async () => {
-//     try {
-//       let accounts = await window.LeatherProvider.authenticationRequest();
-//     }
-//     catch {
-//       console.log('Leather Connection failed')
-//     }
-//   }
-
-
-
-//   const connectPhantom = async () => {
-//     console.log(window);
-//   }
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <p>
-//           LiteScribe wallet tester
-//         </p>
-//         <button onClick={() => { connectWallet() }}>
-//           Wallet
-//         </button>
-
-//         <button onClick={() => { connectUnisat() }}>
-//           Connect Unisat
-//         </button>
-
-
-//         <button onClick={() => { connectXverse() }}>
-//           Connect Xverse
-//         </button>
-
-//         <button onClick={() => { connectLeather() }}>
-//           Connect Leather
-//         </button>
-
-//         <button onClick={() => { connectPhantom() }}>
-//           Connect Phantom
-//         </button>
-//         <button onClick={() => { window.litescribe.initialize() }}>
-//           Disconnect
-//         </button>
-
-//         <p>
-//           <label>
-//             Address<br />
-//           </label>
-//           <input
-//             value={accounts[0]}
-//           >
-//           </input>
-//         </p>
-
-//         <p>
-//           <label>
-//             Network<br />
-//           </label>
-//           <input
-//             value={network}
-//             disabled={true}
-//           >
-//           </input>
-//         </p>
-//         <p>
-//           <label>
-//             Public Key<br />
-//           </label>
-//           <input
-//             value={publicKey}
-//             disabled={true}
-//           >
-//           </input>
-//         </p>
-//         <p>
-//           <label>
-//             Balance<br />
-//           </label>
-//           <input
-//             value={balance?.total}
-//             disabled={true}
-//           >
-//           </input>
-//         </p>
-//       </header>
-//       <body>
-
-//       </body>
-//     </div>
-//   );
-// }
+  //       </body>
+  //     </div>
+  //   );
+}
 
 // export default App;
